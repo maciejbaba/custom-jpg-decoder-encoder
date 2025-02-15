342a59cf1b38f31aaacfd758b039c0d43d885826
Maciej Baba <maciek2baba2@gmail.com>
2025-02-15 18:10:42 +0100

    add tsconfig, verify jpg signature

--- index.ts
+++ index.ts
@@ -1,5 +1,20 @@
-import { readFileSync } from "fs";
+import { readFileSync } from 'fs';
 
-const input = readFileSync("input.txt", "utf-8");
+const JPG_SIGNATURE = [0xff, 0xd8, 0xff];
+const SOF_MARKERS = [0xc0, 0xc2]; // baseline and progressive dct
 
-console.log(input);
+const input = readFileSync('test.jpg');
+const buffer = Buffer.from(input);
+const data = new Uint8Array(buffer);
+console.log('🚀 ~ data:', data);
+
+const signature = data.slice(0, 3);
+console.log('🚀 ~ signature:', signature);
+
+if (signature.every((value, index) => value === JPG_SIGNATURE[index])) {
+  console.log('Valid JPG signature');
+}
+
+const bytes = data.slice(3);
+console.log('🚀 ~ bytes:', bytes);
+
